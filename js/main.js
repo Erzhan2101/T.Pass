c597f3a10447d1b63c60c6c4ee7333d7ebcce218
Erzhan2101 <erzhan210198@gmail.com>
2021-06-22 19:27:36 +0600

    owl-corousel,footer,connection

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop: true,
         items: 1,
-        center: true,
         margin: 30,
-        stagePadding: 300
+
     });
 });
