fafab4381b792a477b64a1cbe1d5288062be9875
Erzhan2101 <erzhan210198@gmail.com>
2021-06-27 15:12:53 +0600

    media

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const topMenu = document.querySelector('.menu')
+const  mobileMenu = document.querySelector('.mobile-menu')
+
+mobileMenu.addEventListener('click', () => {
+    topMenu.classList.toggle('show-menu')
+})
+
+const menuLinks = document.querySelectorAll('.menu li a')
+
+menuLinks.forEach(element => element.addEventListener('click', () => {
+    topMenu.classList.remove('show-menu')
+}))
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop: true,
@@ -6,3 +19,4 @@ $(document).ready(function(){
 
     });
 });
+
